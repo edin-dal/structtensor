55305dee671d7ca371f68e6cc23cffb6c13367ba
mtghorbani98@gmail.com <mtghorbani98@gmail.com>
2023-05-09 04:41:18 +0100

    LRC, PR2C, and PR3C MLIR added

--- CodeGenerator/structtensor/src/main/scala/Main.scala
+++ CodeGenerator/structtensor/src/main/scala/Main.scala
@@ -6680,6 +6680,108 @@ s"""
     write2File(s"outputs_mlir/$outName.mlir", code)
   }
 
+  def LRC_MLIR(sparse: Boolean = false) = {
+    val outName = if (sparse) "LRC_Sparse" else "LRC"
+    val c1 = MLIR_init_code()
+    val argv_names = Seq("CONT_SZ")
+    val c2 = MLIR_read_argv(argv_names)
+    val c3 = s"""
+    %cont_sum1 = memref.alloc(%CONT_SZ) : memref<?xf64>
+    %cont_degree2 = memref.alloc(%CONT_SZ, %CONT_SZ) : memref<?x?xf64>
+"""
+    val c4 = MLIR_gen_random_number("cont_sum1", Seq("CONT_SZ"), getDefaultCondition("oi1"))
+    val c5 = MLIR_gen_random_number("cont_degree2", Seq("CONT_SZ", "CONT_SZ"), getDefaultCondition("oi0"))
+    val c6 = MLIR_start_timer_code()
+    val allDegs = (2 to 2)
+    val (const_tensorComputation, const_dimInfo, const_uniqueSets, const_redundancyMap) = allDegs.foldLeft((Seq.empty[Rule], Seq.empty[DimInfo], Map.empty[Exp, Rule], Map.empty[Exp, Rule]))((acc, d) => {
+      val res = e2eConstructor(d)
+      (acc._1 :+ res._1, acc._2 ++ res._2, mergeMap(Seq(acc._3, res._3))((v1, v2) => v2), mergeMap(Seq(acc._4, res._4))((v1, v2) => v2))
+    })
+    val c7 = const_tensorComputation.foldLeft("")((acc, ctc) => s"$acc\n${codeGen(ctc, const_dimInfo, const_uniqueSets, const_redundancyMap, 1, sparse, codeLang="MLIR")}")
+    val cerr_load = s"""
+    %last = "memref.load"(%cont_degree2, %0, %0) : (memref<?x?xf64>, index, index) -> f64
+"""
+    val c8 = MLIR_end_code(cerr_load)
+    val code = s"$c1\n$c2\n$c3\n$c4\n$c5\n$c6\n$c7\n$c8"
+    write2File(s"outputs_mlir/$outName.mlir", code)
+  }
+
+  def PR2C_MLIR(sparse: Boolean = false) = {
+    val outName = if (sparse) "PR2C_Sparse" else "PR2C"
+    val c1 = MLIR_init_code()
+    val argv_names = Seq("CONT_SZ")
+    val c2 = MLIR_read_argv(argv_names)
+    val c3 = s"""
+    %cont_sum1 = memref.alloc(%CONT_SZ) : memref<?xf64>
+    %cont_degree2 = memref.alloc(%CONT_SZ, %CONT_SZ) : memref<?x?xf64>
+    %cont_degree3 = memref.alloc(%CONT_SZ, %CONT_SZ, %CONT_SZ) : memref<?x?x?xf64>
+    %cont_degree4 = memref.alloc(%CONT_SZ, %CONT_SZ, %CONT_SZ, %CONT_SZ) : memref<?x?x?x?xf64>
+"""
+    val c4 = MLIR_gen_random_number("cont_sum1", Seq("CONT_SZ"), getDefaultCondition("oi1"))
+    val c5 = MLIR_gen_random_number("cont_degree2", Seq("CONT_SZ", "CONT_SZ"), getDefaultCondition("oi0"))
+    val c6 = MLIR_gen_random_number("cont_degree3", Seq("CONT_SZ", "CONT_SZ", "CONT_SZ"), getDefaultCondition("oi0"))
+    val c7 = MLIR_gen_random_number("cont_degree4", Seq("CONT_SZ", "CONT_SZ", "CONT_SZ", "CONT_SZ"), getDefaultCondition("oi0"))
+    val c8 = MLIR_start_timer_code()
+    val allDegs = (2 to 4)
+    val (const_tensorComputation, const_dimInfo, const_uniqueSets, const_redundancyMap) = allDegs.foldLeft((Seq.empty[Rule], Seq.empty[DimInfo], Map.empty[Exp, Rule], Map.empty[Exp, Rule]))((acc, d) => {
+      val res = e2eConstructor(d)
+      (acc._1 :+ res._1, acc._2 ++ res._2, mergeMap(Seq(acc._3, res._3))((v1, v2) => v2), mergeMap(Seq(acc._4, res._4))((v1, v2) => v2))
+    })
+    val c9 = const_tensorComputation.foldLeft("")((acc, ctc) => s"$acc\n${codeGen(ctc, const_dimInfo, const_uniqueSets, const_redundancyMap, 1, sparse, codeLang="MLIR")}")
+    val cerr_load = s"""
+    %last2 = "memref.load"(%cont_degree2, %0, %0) : (memref<?x?xf64>, index, index) -> f64
+    "func.call"(%last2) {callee = @print_f64_cerr} : (f64) -> ()
+    %last3 = "memref.load"(%cont_degree3, %0, %0, %0) : (memref<?x?x?xf64>, index, index, index) -> f64
+    "func.call"(%last3) {callee = @print_f64_cerr} : (f64) -> ()
+    %last = "memref.load"(%cont_degree4, %0, %0, %0, %0) : (memref<?x?x?x?xf64>, index, index, index, index) -> f64
+"""
+    val c10 = MLIR_end_code(cerr_load)
+    val code = s"$c1\n$c2\n$c3\n$c4\n$c5\n$c6\n$c7\n$c8\n$c9\n$c10"
+    write2File(s"outputs_mlir/$outName.mlir", code)
+  }
+
+  def PR3C_MLIR(sparse: Boolean = false) = {
+    val outName = if (sparse) "PR3C_Sparse" else "PR3C"
+    val c1 = MLIR_init_code()
+    val argv_names = Seq("CONT_SZ")
+    val c2 = MLIR_read_argv(argv_names)
+    val c3 = s"""
+    %cont_sum1 = memref.alloc(%CONT_SZ) : memref<?xf64>
+    %cont_degree2 = memref.alloc(%CONT_SZ, %CONT_SZ) : memref<?x?xf64>
+    %cont_degree3 = memref.alloc(%CONT_SZ, %CONT_SZ, %CONT_SZ) : memref<?x?x?xf64>
+    %cont_degree4 = memref.alloc(%CONT_SZ, %CONT_SZ, %CONT_SZ, %CONT_SZ) : memref<?x?x?x?xf64>
+    %cont_degree5 = memref.alloc(%CONT_SZ, %CONT_SZ, %CONT_SZ, %CONT_SZ, %CONT_SZ) : memref<?x?x?x?x?xf64>
+    %cont_degree6 = memref.alloc(%CONT_SZ, %CONT_SZ, %CONT_SZ, %CONT_SZ, %CONT_SZ, %CONT_SZ) : memref<?x?x?x?x?x?xf64>
+"""
+    val c4 = MLIR_gen_random_number("cont_sum1", Seq("CONT_SZ"), getDefaultCondition("oi1"))
+    val c5 = MLIR_gen_random_number("cont_degree2", Seq("CONT_SZ", "CONT_SZ"), getDefaultCondition("oi0"))
+    val c6 = MLIR_gen_random_number("cont_degree3", Seq("CONT_SZ", "CONT_SZ", "CONT_SZ"), getDefaultCondition("oi0"))
+    val c7 = MLIR_gen_random_number("cont_degree4", Seq("CONT_SZ", "CONT_SZ", "CONT_SZ", "CONT_SZ"), getDefaultCondition("oi0"))
+    val c8 = MLIR_gen_random_number("cont_degree5", Seq("CONT_SZ", "CONT_SZ", "CONT_SZ", "CONT_SZ", "CONT_SZ"), getDefaultCondition("oi0"))
+    val c9 = MLIR_gen_random_number("cont_degree6", Seq("CONT_SZ", "CONT_SZ", "CONT_SZ", "CONT_SZ", "CONT_SZ", "CONT_SZ"), getDefaultCondition("oi0"))
+    val c10 = MLIR_start_timer_code()
+    val allDegs = (2 to 6)
+    val (const_tensorComputation, const_dimInfo, const_uniqueSets, const_redundancyMap) = allDegs.foldLeft((Seq.empty[Rule], Seq.empty[DimInfo], Map.empty[Exp, Rule], Map.empty[Exp, Rule]))((acc, d) => {
+      val res = e2eConstructor(d)
+      (acc._1 :+ res._1, acc._2 ++ res._2, mergeMap(Seq(acc._3, res._3))((v1, v2) => v2), mergeMap(Seq(acc._4, res._4))((v1, v2) => v2))
+    })
+    val c11 = const_tensorComputation.foldLeft("")((acc, ctc) => s"$acc\n${codeGen(ctc, const_dimInfo, const_uniqueSets, const_redundancyMap, 1, sparse, codeLang="MLIR")}")
+    val cerr_load = s"""
+    %last2 = "memref.load"(%cont_degree2, %0, %0) : (memref<?x?xf64>, index, index) -> f64
+    "func.call"(%last2) {callee = @print_f64_cerr} : (f64) -> ()
+    %last3 = "memref.load"(%cont_degree3, %0, %0, %0) : (memref<?x?x?xf64>, index, index, index) -> f64
+    "func.call"(%last3) {callee = @print_f64_cerr} : (f64) -> ()
+    %last4 = "memref.load"(%cont_degree4, %0, %0, %0, %0) : (memref<?x?x?x?xf64>, index, index, index, index) -> f64
+    "func.call"(%last4) {callee = @print_f64_cerr} : (f64) -> ()
+    %last5 = "memref.load"(%cont_degree5, %0, %0, %0, %0, %0) : (memref<?x?x?x?x?xf64>, index, index, index, index, index) -> f64
+    "func.call"(%last5) {callee = @print_f64_cerr} : (f64) -> ()
+    %last = "memref.load"(%cont_degree6, %0, %0, %0, %0, %0, %0) : (memref<?x?x?x?x?x?xf64>, index, index, index, index, index, index) -> f64
+"""
+    val c12 = MLIR_end_code(cerr_load)
+    val code = s"$c1\n$c2\n$c3\n$c4\n$c5\n$c6\n$c7\n$c8\n$c9\n$c10\n$c11\n$c12"
+    write2File(s"outputs_mlir/$outName.mlir", code)
+  }
+
 
   val help: String = s"""
 Please specify the experiment name and select whether you want sparse datalayout for the input [use `sbt "run <exp> [sparse]"`]:
@@ -6733,9 +6835,9 @@ PGLM      = Population Growth Leslie Matrix
       }
     } else if (codeLang == "MLIR") {
       args(0) match {
-        // case "LRC" => LRC_MLIR()
-        // case "PR2C" => PR2C_MLIR()
-        // case "PR3C" => PR3C_MLIR()
+        case "LRC" => LRC_MLIR()
+        case "PR2C" => PR2C_MLIR()
+        case "PR3C" => PR3C_MLIR()
         // case "LRA" => LRA_MLIR()
         // case "PR2A" => PR2A_MLIR()
         // case "PR3A" => PR3A_MLIR()
