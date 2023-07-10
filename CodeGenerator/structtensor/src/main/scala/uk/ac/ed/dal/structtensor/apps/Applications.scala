a5faef778f0c9c721356c5d63e47cab2933309f5
mtghorbani98@gmail.com <mtghorbani98@gmail.com>
2023-07-11 00:43:19 +0100

    PGLM fixed

--- CodeGenerator/structtensor/src/main/scala/uk/ac/ed/dal/structtensor/apps/Applications.scala
+++ CodeGenerator/structtensor/src/main/scala/uk/ac/ed/dal/structtensor/apps/Applications.scala
@@ -3231,15 +3231,21 @@ s"""
     val head: Access = Access("M", Seq("i".toVar), Tensor)
     val var1: Access = Access("L",  Seq("i".toVar, "j".toVar), Tensor)
     val var2: Access = Access("N",  Seq("j".toVar), Tensor)
+    val var3: Access = Access("L1",  Seq("i".toVar, "j".toVar), Tensor)
+    val var4: Access = Access("L2",  Seq("i".toVar, "j".toVar), Tensor)
     
 
     val prods: Prod = Prod(Seq(var1, var2))
-    val body: SoP = SoP(Seq(prods))
+    val prods1: Prod = Prod(Seq(var3, var2))
+    val prods2: Prod = Prod(Seq(var4, var2))
+    val body: SoP = if (sparse) SoP(Seq(prods1, prods2)) else SoP(Seq(prods))
     val tensorComputation: Rule = Rule(head, body)
 
     val dim1: DimInfo = DimInfo(var1, Seq("W".toVar, "W".toVar))
     val dim2: DimInfo = DimInfo(var2, Seq("W".toVar))
-    val dimInfo: Seq[DimInfo] = Seq(dim1, dim2)
+    val dim3: DimInfo = DimInfo(var3, Seq("W".toVar, "W".toVar))
+    val dim4: DimInfo = DimInfo(var4, Seq("W".toVar, "W".toVar))
+    val dimInfo: Seq[DimInfo] = if (sparse) Seq(dim2, dim3, dim4) else Seq(dim1, dim2)
 
 
     val var1HeadUS: Access = Access(var1.name.uniqueName, var1.vars, UniqueSet)
@@ -3255,8 +3261,6 @@ s"""
     val var1BodyRM: SoP = emptySoP()
     val var1RM: Rule = Rule(var1HeadRM, var1BodyRM)
 
-    val var1DL: Function[Seq[Variable], Seq[Index]] = (x: Seq[Variable]) => x
-
     val var2HeadUS: Access = Access(var2.name.uniqueName, var2.vars, UniqueSet)
     val var2BodyUS: SoP = dim2.toSoP
     val var2US: Rule = Rule(var2HeadUS, var2BodyUS)
@@ -3265,9 +3269,34 @@ s"""
     val var2BodyRM: SoP = emptySoP()
     val var2RM: Rule = Rule(var2HeadRM, var2BodyRM)
 
-    val uniqueSets: Map[Exp, Rule] = Map(var1 -> var1US, var2 -> var2US)
-    val redundancyMap: Map[Exp, Rule] = Map(var1 -> var1RM, var2 -> var2RM)
-    val dataLayoutMap: Map[Exp, Function[Seq[Variable], Seq[Index]]] = if (sparse) Map(var1 -> var1DL) else Map()
+    val var3HeadUS: Access = Access(var3.name.uniqueName, var3.vars, UniqueSet)
+    val var3ExpUS1: Exp = Comparison("=", ConstantInt(0), "i".toVar)
+    val var3ProdsUS1: SoP = SoP(Seq(Prod(Seq(var3ExpUS1))))
+    val var3BodyUS: SoP = multSoP(Seq(dim3.toSoP, var3ProdsUS1))
+    val var3US: Rule = Rule(var3HeadUS, var3BodyUS)
+    
+    val var3HeadRM: Access = Access(var3.name.redundancyName, var3.vars.redundancyVarsInplace, RedundancyMap)
+    val var3BodyRM: SoP = emptySoP()
+    val var3RM: Rule = Rule(var3HeadRM, var3BodyRM)
+
+    val var3DL: Function[Seq[Variable], Seq[Index]] = (x: Seq[Variable]) => Seq(x(0))
+
+    val var4HeadUS: Access = Access(var4.name.uniqueName, var4.vars, UniqueSet)
+    val var4ExpUS1: Exp = Comparison("<=", ConstantInt(1), "i".toVar)
+    val var4ExpUS2: Exp = Comparison("=", Arithmetic("-", "i".toVar, ConstantInt(1)), "j".toVar)
+    val var4ProdsUS1: SoP = SoP(Seq(Prod(Seq(var4ExpUS1, var4ExpUS2))))
+    val var4BodyUS: SoP = multSoP(Seq(dim4.toSoP, var4ProdsUS1))
+    val var4US: Rule = Rule(var4HeadUS, var4BodyUS)
+
+    val var4HeadRM: Access = Access(var4.name.redundancyName, var4.vars.redundancyVarsInplace, RedundancyMap)
+    val var4BodyRM: SoP = emptySoP()
+    val var4RM: Rule = Rule(var4HeadRM, var4BodyRM)
+
+    val var4DL: Function[Seq[Variable], Seq[Index]] = (x: Seq[Variable]) => Seq(x(1))
+
+    val uniqueSets: Map[Exp, Rule] = if (sparse) Map(var2 -> var2US, var3 -> var3US, var4 -> var4US) else Map(var1 -> var1US, var2 -> var2US)
+    val redundancyMap: Map[Exp, Rule] = if (sparse) Map(var2 -> var2RM, var3 -> var3RM, var4 -> var4RM) else Map(var1 -> var1RM, var2 -> var2RM)
+    val dataLayoutMap: Map[Exp, Function[Seq[Variable], Seq[Index]]] = if (sparse) Map(var3 -> var3DL, var4 -> var4DL) else Map()
 
     if (mode == 0) {
       println(codeGen(tensorComputation, dimInfo, uniqueSets, redundancyMap, dataLayoutMap=dataLayoutMap))
