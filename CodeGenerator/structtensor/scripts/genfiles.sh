77f28bc253a25af4a8fe0bf82cd5c68d331f88ee
Emilien Bauer <bauer.emilien@gmail.com>
2023-09-12 17:29:14 +0100

    Optimize genfiles, one sbt run with many commands instead of many sbt runs.

--- CodeGenerator/structtensor/scripts/genfiles.sh
+++ CodeGenerator/structtensor/scripts/genfiles.sh
@@ -2,9 +2,14 @@
 
 cd ..
 structtensor_names=("TTM_DP" "TTM_J" "THP_DP" "THP_I" "THP_J" "MTTKRP_IJ" "MTTKRP_I" "MTTKRP_J")
+
+sbt_commands=""
+
 for name in ${structtensor_names[@]}
 do
-  sbt "run $name"
-  sbt "run $name sparse"
-  sbt "run $name dense MLIR"
+  sbt_commands="$sbt_commands;run $name"
+  sbt_commands="$sbt_commands;run $name sparse"
+  sbt_commands="$sbt_commands;run $name dense MLIR"
 done
+
+sbt "$sbt_commands"
