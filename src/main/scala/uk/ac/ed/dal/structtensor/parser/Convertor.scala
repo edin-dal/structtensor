4722d9b581b028c2865300697c302bf6cb71eff2
mtghorbani <mtghorbani98@gmail.com>
2023-11-03 21:48:31 +0000

    same name dimInfo/uniqueSet problem solved

--- src/main/scala/uk/ac/ed/dal/structtensor/parser/Convertor.scala
+++ src/main/scala/uk/ac/ed/dal/structtensor/parser/Convertor.scala
@@ -10,30 +10,30 @@ object Convertor {
   import STURHelper._
   import Bodygen._
 
-  def groupRules(rules: Seq[Rule]): (Map[String, Rule], Map[String, Rule], Map[String, Rule], Map[String, Rule]) = {
-    rules.foldLeft((Map.empty[String, Rule], Map.empty[String, Rule], Map.empty[String, Rule], Map.empty[String, Rule]))((acc, r) => {
-      val nameToTensorMap = acc._1
-      val nameToUniqueSetMap = acc._2
-      val nameToRedundancyMapMap = acc._3
-      val nameToDimensionMap = acc._4
-      val name = r.head.name
+  def groupRules(rules: Seq[Rule]): (Map[Access, Rule], Map[Access, Rule], Map[Access, Rule], Map[Access, Rule]) = {
+    rules.foldLeft((Map.empty[Access, Rule], Map.empty[Access, Rule], Map.empty[Access, Rule], Map.empty[Access, Rule]))((acc, r) => {
+      val headToTensorMap = acc._1
+      val headToUniqueSetMap = acc._2
+      val headToRedundancyMapMap = acc._3
+      val headToDimensionMap = acc._4
+      val head = Access(r.head.name, r.head.vars, Tensor)
       r.head.kind match {
-        case Tensor => (nameToTensorMap ++ Map(name -> r), nameToUniqueSetMap, nameToRedundancyMapMap, nameToDimensionMap)
-        case UniqueSet => (nameToTensorMap, nameToUniqueSetMap ++ Map(name -> r), nameToRedundancyMapMap, nameToDimensionMap)
-        case RedundancyMap => (nameToTensorMap, nameToUniqueSetMap, nameToRedundancyMapMap ++ Map(name -> r), nameToDimensionMap)
-        case DimensionType => (nameToTensorMap, nameToUniqueSetMap, nameToRedundancyMapMap, nameToDimensionMap ++ Map(name -> r))
+        case Tensor => (headToTensorMap ++ Map(head -> r), headToUniqueSetMap, headToRedundancyMapMap, headToDimensionMap)
+        case UniqueSet => (headToTensorMap, headToUniqueSetMap ++ Map(head -> r), headToRedundancyMapMap, headToDimensionMap)
+        case RedundancyMap => (headToTensorMap, headToUniqueSetMap, headToRedundancyMapMap ++ Map(head -> r), headToDimensionMap)
+        case DimensionType => (headToTensorMap, headToUniqueSetMap, headToRedundancyMapMap, headToDimensionMap ++ Map(head -> r))
         case _ => throw new Exception("Unknown kind of tensor")
       }
     })
   }
 
-  def getNamesThatRequireDim(nameToTensorMap: Map[String, Rule]): Seq[String] = {
-    val lhs: Seq[String] = nameToTensorMap.keySet.toSeq
-    val rhs: Seq[String] = nameToTensorMap.values.foldLeft(Seq.empty[String])((acc, r) => {
-      acc ++ r.body.prods.foldLeft(Seq.empty[String])((acc2, p) => {
-        acc2 ++ p.exps.foldLeft(Seq.empty[String])((acc3, e) => {
+  def getHeadsThatRequireDim(headToTensorMap: Map[Access, Rule]): Seq[Access] = {
+    val lhs: Seq[Access] = headToTensorMap.keySet.toSeq
+    val rhs: Seq[Access] = headToTensorMap.values.foldLeft(Seq.empty[Access])((acc, r) => {
+      acc ++ r.body.prods.foldLeft(Seq.empty[Access])((acc2, p) => {
+        acc2 ++ p.exps.foldLeft(Seq.empty[Access])((acc3, e) => {
           e match {
-            case Access(n, _, _) => acc3 :+ n
+            case Access(n, v, _) => acc3 :+ Access(n, v, Tensor)
             case _ => acc3
           }
         })
@@ -42,9 +42,9 @@ object Convertor {
     rhs.diff(lhs).distinct.toSeq
   }
 
-  def checkDimsAvailable(nameToTensorMap: Map[String, Rule], nameToDimensionMap: Map[String, Rule]): Boolean = {
-    getNamesThatRequireDim(nameToTensorMap).foldLeft(true)((acc, name) => {
-      nameToDimensionMap.get(name) match {
+  def checkDimsAvailable(headToTensorMap: Map[Access, Rule], headToDimensionMap: Map[Access, Rule]): Boolean = {
+    getHeadsThatRequireDim(headToTensorMap).foldLeft(true)((acc, head) => {
+      headToDimensionMap.get(head) match {
         case None => false
         case Some(d) => acc
       }
@@ -79,28 +79,28 @@ object Convertor {
     upperBounds(0)
   }
 
-  def extractDims(nameToDimensionMap: Map[String, Rule]): (Seq[DimInfo], Map[String, DimInfo]) = { // and a map to diminfo to mix it with unique set and redundancy map if necessary.
-    nameToDimensionMap.foldLeft((Seq.empty[DimInfo], Map.empty[String, DimInfo]))((acc, nameRule) => {
-      val name = nameRule._1
-      val rule = nameRule._2
+  def extractDims(headToDimensionMap: Map[Access, Rule]): (Seq[DimInfo], Map[Access, DimInfo]) = { // and a map to diminfo to mix it with unique set and redundancy map if necessary.
+    headToDimensionMap.foldLeft((Seq.empty[DimInfo], Map.empty[Access, DimInfo]))((acc, headRule) => {
+      val old_head = headRule._1
+      val rule = headRule._2
       val head = Access(rule.head.name, rule.head.vars, Tensor)
       val body = rule.body
       val dimSeq = head.vars.foldLeft(Seq.empty[Dim])((acc2, v) => acc2 :+ findUpperBound(v, body))
-      (acc._1 :+ DimInfo(head, dimSeq), acc._2 ++ Map(name -> DimInfo(rule.head, dimSeq)))
+      (acc._1 :+ DimInfo(head, dimSeq), acc._2 ++ Map(old_head -> DimInfo(rule.head, dimSeq)))
     })
   }
 
-  def extractSet(nameToSetMap: Map[String, Rule], dimInfoMap: Map[String, DimInfo], kind: AccessType): Map[Exp, Rule] = {
-    dimInfoMap.foldLeft(Map.empty[Exp, Rule])((acc, nameDimInfo) => {
-      val name = nameDimInfo._1
-      val dimInfo = nameDimInfo._2
+  def extractSet(headToSetMap: Map[Access, Rule], dimInfoMap: Map[Access, DimInfo], kind: AccessType): Map[Exp, Rule] = {
+    dimInfoMap.foldLeft(Map.empty[Exp, Rule])((acc, headDimInfo) => {
+      val old_head = headDimInfo._1
+      val dimInfo = headDimInfo._2
       val head = Access(dimInfo.access.name, dimInfo.access.vars, Tensor)
       val body = dimInfo.toSoP
-      val newBody = if (kind == UniqueSet) nameToSetMap.get(name) match {
+      val newBody = if (kind == UniqueSet) headToSetMap.get(old_head) match {
         case Some(r) => SoPTimesSoP(body, r.body)
         case None => body
       } else {
-        nameToSetMap.get(name) match {
+        headToSetMap.get(old_head) match {
           case Some(r) => SoPTimesSoP(body, r.body)
           case None => emptySoP()
         }
@@ -123,23 +123,23 @@ object Convertor {
   }
 
   def convertRules(rules: Seq[Rule], initTensors: Boolean, enforceDimensions: Boolean, codeLang: String, sturOpt: Boolean): (String, Seq[Rule], Seq[DimInfo], Map[Exp, Rule], Map[Exp, Rule], String) = {
-    val (nameToTensorMap, nameToUniqueSetMap, nameToRedundancyMapMap, nameToDimensionMap) = groupRules(rules)
-    val dimsAvailable = checkDimsAvailable(nameToTensorMap, nameToDimensionMap)
+    val (headToTensorMap, headToUniqueSetMap, headToRedundancyMapMap, headToDimensionMap) = groupRules(rules)
+    val dimsAvailable = checkDimsAvailable(headToTensorMap, headToDimensionMap)
     if (!dimsAvailable) throw new Exception("Dimensions not available for all tensors")
-    val (dimInfo, dimInfoMap): (Seq[DimInfo], Map[String, DimInfo]) = extractDims(nameToDimensionMap)
-    val uniqueSets: Map[Exp, Rule] = if (!enforceDimensions) nameToUniqueSetMap.values.map(r => (r.head -> Rule(Access(r.head.name, r.head.vars, Tensor), r.body))).toMap else extractSet(nameToUniqueSetMap, dimInfoMap, UniqueSet)
-    val redundancyMaps: Map[Exp, Rule] = if (!enforceDimensions) nameToRedundancyMapMap.values.map(r => (r.head -> Rule(Access(r.head.name, r.head.vars, Tensor), r.body))).toMap else extractSet(nameToRedundancyMapMap, dimInfoMap, RedundancyMap)
-    val tensorComputations: Seq[Rule] = nameToTensorMap.values.toSeq
-    // println("Enforce Dimensions:")
-    // println(enforceDimensions)
-    // println("Tensor Computations:")
-    // tensorComputations.map(r => println(r.prettyFormat))
-    // println("Unique Sets:")
-    // uniqueSets.values.toSeq.map(r => println(r.prettyFormat))
-    // println("Redundancy Maps:")
-    // redundancyMaps.values.toSeq.map(r => println(r.prettyFormat))
-    // println("Dimensions:")
-    // println(dimInfo)
+    val (dimInfo, dimInfoMap): (Seq[DimInfo], Map[Access, DimInfo]) = extractDims(headToDimensionMap)
+    val uniqueSets: Map[Exp, Rule] = if (!enforceDimensions) headToUniqueSetMap.values.map(r => (r.head -> Rule(Access(r.head.name, r.head.vars, Tensor), r.body))).toMap else extractSet(headToUniqueSetMap, dimInfoMap, UniqueSet)
+    val redundancyMaps: Map[Exp, Rule] = if (!enforceDimensions) headToRedundancyMapMap.values.map(r => (r.head -> Rule(Access(r.head.name, r.head.vars, Tensor), r.body))).toMap else extractSet(headToRedundancyMapMap, dimInfoMap, RedundancyMap)
+    val tensorComputations: Seq[Rule] = headToTensorMap.values.toSeq
+    println("Enforce Dimensions:")
+    println(enforceDimensions)
+    println("Tensor Computations:")
+    tensorComputations.map(r => println(r.prettyFormat))
+    println("Unique Sets:")
+    uniqueSets.values.toSeq.map(r => println(r.prettyFormat))
+    println("Redundancy Maps:")
+    redundancyMaps.values.toSeq.map(r => println(r.prettyFormat))
+    println("Dimensions:")
+    println(dimInfo)
     val all_dimensions: Seq[DimInfo] = dimInfo ++ tensorComputations.map(r => infer(r, dimInfo, uniqueSets, redundancyMaps)._4)
     val all_tensors: Seq[Access] = getAllTensors(tensorComputations).distinct
     val (init, end) = if (!initTensors) ("", "") else Bodygen(codeLang, rules, all_tensors, all_dimensions.toAccessMap, uniqueSets, sturOpt)
