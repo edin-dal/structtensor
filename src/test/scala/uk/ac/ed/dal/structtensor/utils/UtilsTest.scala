4e211913b66668630519e3e58b192ec9647aad58
mtghorbani <mtghorbani98@gmail.com>
2024-06-25 19:58:01 +0100

    add test for write2File

--- src/test/scala/uk/ac/ed/dal/structtensor/utils/UtilsTest.scala
+++ src/test/scala/uk/ac/ed/dal/structtensor/utils/UtilsTest.scala
@@ -10,4 +10,16 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.ParallelTestExecution
 
-class UtilsTest extends AnyFlatSpec with Matchers with ParallelTestExecution {}
+class UtilsTest extends AnyFlatSpec with Matchers with ParallelTestExecution {
+  "Utils" should "write to file correct value" in {
+    Utils.write2File("test.txt", "test", append = false)
+    val file = new File("test.txt")
+    file.exists() should be(true)
+    val source = scala.io.Source.fromFile("test.txt")
+    val lines =
+      try source.mkString
+      finally source.close()
+    lines should be("test")
+    file.delete()
+  }
+}
