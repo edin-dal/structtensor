797f0445e7ac3c24a0d5111e8615bd0f6b2d5ad4
mtghorbani <mtghorbani98@gmail.com>
2024-06-25 20:02:11 +0100

    add test for emptyExps and prodTimesProd

--- src/test/scala/uk/ac/ed/dal/structtensor/utils/UtilsTest.scala
+++ src/test/scala/uk/ac/ed/dal/structtensor/utils/UtilsTest.scala
@@ -11,7 +11,7 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.ParallelTestExecution
 
 class UtilsTest extends AnyFlatSpec with Matchers with ParallelTestExecution {
-  "Utils" should "write to file correct value" in {
+  "Utils" should "write correct values to file" in {
     Utils.write2File("test.txt", "test", append = false)
     val file = new File("test.txt")
     file.exists() should be(true)
@@ -26,5 +26,35 @@ class UtilsTest extends AnyFlatSpec with Matchers with ParallelTestExecution {
   it should "return correct variable name" in {
     Utils.getVar("test") should be("test1")
     Utils.getVar("test") should be("test2")
+  }
+
+  it should "return empty Prod" in {
+    Utils.emptyProd() should be(Prod(Seq[Exp]()))
+  }
+
+  it should "return empty SoP" in {
+    Utils.emptySoP() should be(SoP(Seq[Prod]()))
+  }
+
+  it should "return empty Access" in {
+    Utils.emptyAccess() should be(Access("", Seq[Variable](), Tensor))
+  }
+
+  it should "return empty Rule" in {
+    Utils.emptyRule() should be(Rule(Utils.emptyAccess(), Utils.emptySoP()))
+  }
+
+  it should "return correct Prod times Prod" in {
+    Utils.prodTimesProd(
+      Prod(Seq[Exp](Access("x", Seq[Variable](), Tensor))),
+      Prod(Seq[Exp](Access(("y"), Seq[Variable](), Tensor)))
+    ) should be(
+      Prod(
+        Seq[Exp](
+          Access("x", Seq[Variable](), Tensor),
+          Access("y", Seq[Variable](), Tensor)
+        )
+      )
+    )
   }
 }
