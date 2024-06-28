883efb719bd9db7d3ebf7febb5fc452be94580d8
mtghorbani <mtghorbani98@gmail.com>
2024-06-28 02:13:03 +0100

    added tests for set idempotence and sub components + fix one denormalizer test

---
+++ src/test/scala/uk/ac/ed/dal/structtensor/compiler/CompilerTest.scala
@@ -0,0 +1,16 @@
+package uk.ac.ed.dal
+package structtensor
+package compiler
+
+import utils._
+import Utils._
+import Optimizer._
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.ParallelTestExecution
+
+class CompilerTest
+    extends AnyFlatSpec
+    with Matchers
+    with ParallelTestExecution {}
