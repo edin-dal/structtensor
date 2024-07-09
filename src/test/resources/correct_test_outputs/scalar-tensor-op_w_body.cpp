41648ae90a72c19f66f555a2393204f85eb7c932
mtghorbani <mtghorbani98@gmail.com>
2024-07-10 00:09:09 +0100

    test for scalar operation added

---
+++ src/test/resources/correct_test_outputs/scalar-tensor-op_w_body.cpp
@@ -0,0 +1,87 @@
+
+#include <iostream>
+#include <random>
+#include <algorithm>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
+
+int main(int argc, char **argv){
+    srand(0);
+
+
+const int N = atoi(argv[1]);
+double *t = new double[N];
+for (size_t i = 0; i < N; ++i) {
+int flag1 = 0 <= i && N > i;
+if (flag1) {
+t[i] = (double) (rand() % 1000000) / 1e6;
+} else {
+t[i] = 0.0;
+}
+}
+double *f = new double[N];
+for (size_t i = 0; i < N; ++i) {
+int flag2 = 0 <= i && N > i;
+if (flag2) {
+f[i] = (double) (rand() % 1000000) / 1e6;
+} else {
+f[i] = 0.0;
+}
+}
+double p = 0.0;
+double **A = new double*[N];
+for (size_t i = 0; i < N; ++i) {
+A[i] = new double[N];
+
+for (size_t j = 0; j < N; ++j) {
+A[i][j] = 0.0;
+}
+}
+
+long time_computation = 0, start_computation, end_computation;
+start_computation = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+for (int i = 0; i < N; ++i) {
+
+p += t[i];
+}
+for (int i = 0; i < N; ++i) {
+
+for (int j = 0; j < min({(i) + 1, N}); ++j) {
+
+for (int i4 = 0; i4 < N; ++i4) {
+
+A[i][j] += (f[j] * f[i] * t[i4]);
+}
+}
+}
+end_computation = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+time_computation = end_computation - start_computation;
+cout << time_computation << endl;
+long time_reconstruction = 0, start_reconstruction, end_reconstruction;
+start_reconstruction = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+
+for (int i = 0; i < N; ++i) {
+
+int jp = i;
+for (int j = max({(i) + 1, 0}); j < N; ++j) {
+
+int ip = j;
+A[i][j] += A[ip][jp];
+}
+}
+end_reconstruction = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+time_reconstruction = end_reconstruction - start_reconstruction;
+cout << time_reconstruction << endl;
+cerr << p << endl;
+cerr << t[0] << endl;
+cerr << A[0][0] << endl;
+cerr << f[0] << endl;
+cerr << f[0] << endl;
+delete[] t;
+for (size_t i0 = 0; i0 < N; ++i0) {delete[] A[i0];
+}delete[] A;
+delete[] f;
+return 0;
+}
--- src/test/scala/uk/ac/ed/dal/structtensor/codegen/CodegenTest.scala
+++ src/test/scala/uk/ac/ed/dal/structtensor/codegen/CodegenTest.scala
@@ -1461,4 +1461,49 @@ class CodegenTest extends AnyFlatSpec with Matchers {
     val lines2 = file2.getLines().toList
     lines2 should be(lines1)
   }
+
+  it should "generate code for when there is a scalar tensor in the computation without the body" in {
+    Utils.cnt = 0
+    Main.main(
+      Array(
+        "-i",
+        "examples/scalar-tensor-op.stur",
+        "-o",
+        "src/test/resources/test_outputs/scalar-tensor-op_wo_body_test.cpp"
+      )
+    )
+
+    val file1 = scala.io.Source.fromFile(
+      "src/test/resources/correct_test_outputs/scalar-tensor-op_wo_body.cpp"
+    )
+    val file2 = scala.io.Source.fromFile(
+      "src/test/resources/test_outputs/scalar-tensor-op_wo_body_test.cpp"
+    )
+    val lines1 = file1.getLines().toList
+    val lines2 = file2.getLines().toList
+    lines2 should be(lines1)
+  }
+
+  it should "generate code for when there is a scalar tensor in the computation with the body" in {
+    Utils.cnt = 0
+    Main.main(
+      Array(
+        "-i",
+        "examples/scalar-tensor-op.stur",
+        "-o",
+        "src/test/resources/test_outputs/scalar-tensor-op_w_body_test.cpp",
+        "--init-tensors"
+      )
+    )
+
+    val file1 = scala.io.Source.fromFile(
+      "src/test/resources/correct_test_outputs/scalar-tensor-op_w_body.cpp"
+    )
+    val file2 = scala.io.Source.fromFile(
+      "src/test/resources/test_outputs/scalar-tensor-op_w_body_test.cpp"
+    )
+    val lines1 = file1.getLines().toList
+    val lines2 = file2.getLines().toList
+    lines2 should be(lines1)
+  }
 }
