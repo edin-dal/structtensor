c201383a7b93040b725ed31d076286b8dfd0e837
mtghorbani <mtghorbani98@gmail.com>
2024-07-10 00:16:16 +0100

    added a test for handling a mixture of constants and scalar tensors

--- src/test/scala/uk/ac/ed/dal/structtensor/codegen/CodegenTest.scala
+++ src/test/scala/uk/ac/ed/dal/structtensor/codegen/CodegenTest.scala
@@ -1551,4 +1551,49 @@ class CodegenTest extends AnyFlatSpec with Matchers {
     val lines2 = file2.getLines().toList
     lines2 should be(lines1)
   }
+
+  it should "generate code for when there is mixture of scalar tensors and constants in the computation without the body" in {
+    Utils.cnt = 0
+    Main.main(
+      Array(
+        "-i",
+        "examples/scalar-constant-mixture.stur",
+        "-o",
+        "src/test/resources/test_outputs/scalar-constant-mixture_wo_body_test.cpp"
+      )
+    )
+
+    val file1 = scala.io.Source.fromFile(
+      "src/test/resources/correct_test_outputs/scalar-constant-mixture_wo_body.cpp"
+    )
+    val file2 = scala.io.Source.fromFile(
+      "src/test/resources/test_outputs/scalar-constant-mixture_wo_body_test.cpp"
+    )
+    val lines1 = file1.getLines().toList
+    val lines2 = file2.getLines().toList
+    lines2 should be(lines1)
+  }
+
+  it should "generate code for when there is mixture of scalar tensors and constants in the computation with the body" in {
+    Utils.cnt = 0
+    Main.main(
+      Array(
+        "-i",
+        "examples/scalar-constant-mixture.stur",
+        "-o",
+        "src/test/resources/test_outputs/scalar-constant-mixture_w_body_test.cpp",
+        "--init-tensors"
+      )
+    )
+
+    val file1 = scala.io.Source.fromFile(
+      "src/test/resources/correct_test_outputs/scalar-constant-mixture_w_body.cpp"
+    )
+    val file2 = scala.io.Source.fromFile(
+      "src/test/resources/test_outputs/scalar-constant-mixture_w_body_test.cpp"
+    )
+    val lines1 = file1.getLines().toList
+    val lines2 = file2.getLines().toList
+    lines2 should be(lines1)
+  }
 }
