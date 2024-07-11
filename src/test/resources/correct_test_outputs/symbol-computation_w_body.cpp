ac2d767fe42adf1c4608cf4d71d73c99586465f0
mtghorbani <mtghorbani98@gmail.com>
2024-07-11 04:08:00 +0100

    added tests where symbol is used as a part of computation

---
+++ src/test/resources/correct_test_outputs/symbol-computation_w_body.cpp
@@ -0,0 +1,50 @@
+
+#include <iostream>
+#include <random>
+#include <algorithm>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
+
+int main(int argc, char **argv){
+    srand(0);
+
+
+const int N = atoi(argv[1]);
+double *B = new double[N];
+for (size_t i = 0; i < N; ++i) {
+int flag1 = 0 <= i && N > i;
+if (flag1) {
+B[i] = (double) (rand() % 1000000) / 1e6;
+} else {
+B[i] = 0.0;
+}
+}
+double *A = new double[N];
+for (size_t i = 0; i < N; ++i) {
+A[i] = 0.0;
+}
+
+long time_computation = 0, start_computation, end_computation;
+start_computation = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+for (int i = 0; i < N; ++i) {
+
+A[i] += (1. / N * B[i]);
+}
+end_computation = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+time_computation = end_computation - start_computation;
+cout << time_computation << endl;
+long time_reconstruction = 0, start_reconstruction, end_reconstruction;
+start_reconstruction = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+
+end_reconstruction = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+time_reconstruction = end_reconstruction - start_reconstruction;
+cout << time_reconstruction << endl;
+cerr << A[0] << endl;
+cerr << B[0] << endl;
+cerr << N << endl;
+delete[] A;
+delete[] B;
+return 0;
+}
--- src/test/scala/uk/ac/ed/dal/structtensor/codegen/CodegenTest.scala
+++ src/test/scala/uk/ac/ed/dal/structtensor/codegen/CodegenTest.scala
@@ -1886,4 +1886,49 @@ class CodegenTest extends AnyFlatSpec with Matchers {
     val lines2 = file2.getLines().toList
     lines2 should be(lines1)
   }
+
+  it should "generate code for when there is a symbol used in the computation without the body" in {
+    Utils.cnt = 0
+    Main.main(
+      Array(
+        "-i",
+        "examples/symbol-computation.stur",
+        "-o",
+        "src/test/resources/test_outputs/symbol-computation_wo_body_test.cpp"
+      )
+    )
+
+    val file1 = scala.io.Source.fromFile(
+      "src/test/resources/correct_test_outputs/symbol-computation_wo_body.cpp"
+    )
+    val file2 = scala.io.Source.fromFile(
+      "src/test/resources/test_outputs/symbol-computation_wo_body_test.cpp"
+    )
+    val lines1 = file1.getLines().toList
+    val lines2 = file2.getLines().toList
+    lines2 should be(lines1)
+  }
+
+  it should "generate code for when there is a symbol used in the computation with the body" in {
+    Utils.cnt = 0
+    Main.main(
+      Array(
+        "-i",
+        "examples/symbol-computation.stur",
+        "-o",
+        "src/test/resources/test_outputs/symbol-computation_w_body_test.cpp",
+        "--init-tensors"
+      )
+    )
+
+    val file1 = scala.io.Source.fromFile(
+      "src/test/resources/correct_test_outputs/symbol-computation_w_body.cpp"
+    )
+    val file2 = scala.io.Source.fromFile(
+      "src/test/resources/test_outputs/symbol-computation_w_body_test.cpp"
+    )
+    val lines1 = file1.getLines().toList
+    val lines2 = file2.getLines().toList
+    lines2 should be(lines1)
+  }
 }
