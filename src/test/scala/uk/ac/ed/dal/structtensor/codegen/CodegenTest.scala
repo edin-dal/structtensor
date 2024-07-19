7276bbb6c13041e72dd0a8cb5b8e4b890fdb9bf4
mtghorbani <mtghorbani98@gmail.com>
2024-07-19 01:31:36 +0100

    add an e2e test for iterator ordering

--- src/test/scala/uk/ac/ed/dal/structtensor/codegen/CodegenTest.scala
+++ src/test/scala/uk/ac/ed/dal/structtensor/codegen/CodegenTest.scala
@@ -2130,4 +2130,26 @@ class CodegenTest extends AnyFlatSpec with Matchers {
     val lines2 = file2.getLines().toList
     lines2 should be(lines1)
   }
+
+  it should "generate code with variable ordering without the body" in {
+    Utils.cnt = 0
+    Main.main(
+      Array(
+        "-i",
+        "examples/variable-ordering.stur",
+        "-o",
+        "src/test/resources/test_outputs/variable-ordering_wo_body_test.cpp"
+      )
+    )
+
+    val file1 = scala.io.Source.fromFile(
+      "src/test/resources/correct_test_outputs/variable-ordering_wo_body.cpp"
+    )
+    val file2 = scala.io.Source.fromFile(
+      "src/test/resources/test_outputs/variable-ordering_wo_body_test.cpp"
+    )
+    val lines1 = file1.getLines().toList
+    val lines2 = file2.getLines().toList
+    lines2 should be(lines1)
+  }
 }
